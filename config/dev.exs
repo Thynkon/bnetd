8a30eb63a7134b1d91a3eabca5619e1b4c393296
thynkon <thynkon@protonmail.com>
2022-10-15 21:49:16 +0200

    Install tailwindcss

--- config/dev.exs
+++ config/dev.exs
@@ -26,7 +26,8 @@ config :bnetd, BnetdWeb.Endpoint,
   secret_key_base: "UnV8hNIShC9IdVMizS9pLuyULvvNZbedAqxo2iOs83oFv2Cw8SFIJ5sWzOiIn91C",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
